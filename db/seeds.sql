8d070c6fc4f6e976469024e90fd2bde217b1c251
Erica  Cohen <ericacohen@Ericas-MacBook-Air.local>
2018-01-16 12:15:22 -0800

    added files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+
+SELECT * FROM burgers;
