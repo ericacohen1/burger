354a9058c5cc90ac577d21d416c08d9bbdca895c
Erica  Cohen <ericacohen@Ericas-Air.lan>
2018-01-18 21:10:44 -0800

    index still in progress

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var app = express();
 app.use(express.static(process.cwd() + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
